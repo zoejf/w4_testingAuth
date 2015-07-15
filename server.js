bddb6417b09970300a7334291c2e6223077977cd
zoejf <zoejfoss@gmail.com>
2015-07-15 16:16:48 -0700

    set up authorization functions

--- server.js
+++ server.js
@@ -1,29 +1,48 @@
+// require express framework and additional modules
 var express = require('express'),
-    app = express();
-    bodyParser = require('body-parser');
+  app = express(),
+  ejs = require('ejs'),
+  bodyParser = require('body-parser'), 
+  mongoose = require('mongoose'),
+  User = require('./models/user');
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(__dirname + '/public'));
+// connect to mongodb
+mongoose.connect('mongodb://localhost/test');
 
-var users = [
-  {
-    name: 'Bob',
-    username: 'bobiscool'
-  },
-  {
-    name: 'Julie',
-    username: 'julierocks'
-  }
-];
+// set view engine for server-side templating
+app.set('view engine', 'ejs');
 
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/public/views/index.html');
+// middleware
+  //body-parser functionality
+  app.use(bodyParser.urlencoded({extended: true}));
+  // set session options
+  app.use(session({
+    saveUninitialized: true,
+    resave: true,
+    secret: 'SuperSecretCookie',
+    cookie: { maxAge: 60000 }
+  }));
+
+// signup route with placeholder response
+app.get('/signup', function (req, res) {
+  res.send('coming soon');
 });
 
-app.get('/users', function(req, res) {
-  res.json(users);
+// user submits the signup form
+app.post('/users', function (req, res) {
+
+  // grab user data from params (req.body)
+  var newUser = req.body.user;
+
+  // create new user with secure password
+  User.createSecure(newUser.email, newUser.password, function (err, user) {
+    res.send(user);
+  });
 });
 
-app.listen(3000, function() {
-  console.log('server started on localhost:3000');
+
+// listen on port 3000
+app.listen(3000, function () {
+  console.log('server started on locahost:3000');
 });
+
